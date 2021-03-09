9087ce3d63a5a8ef7b937c65a56692a323d522b7
Crystal Agoncillo <crystal.agoncillo@gmail.com>
2021-03-08 21:32:16 -0800

    refactor client-side js

--- public/js/app.js
+++ public/js/app.js
@@ -67,65 +67,6 @@ const fetchWeather = (place) => {
 	        	background.classList.add('container', weather)
 	        	weatherStatus = weatherMappings[weather]
 
-
-	     //    	switch(weather) {
-	     //    		case 'partly-cloudy-day':
-		    //     		background.className = ''
-		    //           	background.classList.add('container','partly-cloudy-day')
-		    //           	weatherStatus = 'The weather is currently partly cloudy.<br>'
-		    //           	break
-		    //         case 'partly-cloudy-night':
-		    //         	background.className = ''
-	     //          		background.classList.add('container','partly-cloudy-night')
-	     //          		weatherStatus = 'The weather is currently partly cloudy.<br>'
-	     //          		break
-		    //         case 'cloudy':
-		    //         	background.className = ''
-	     //          		background.classList.add('container','cloudy')
-	     //          		weatherStatus = 'The weather is currently cloudy.<br>'
-	     //          		break
-		    //         case 'wind':
-		    //         	background.className = ''
-	     //          		background.classList.add('container','wind')
-	     //          		weatherStatus = 'The weather is currently windy.<br>'
-	     //          		break
-		    //         case 'rain':
-		    //         	background.className = ''
-	     //          		background.classList.add('container','rain')
-	     //          		weatherStatus = 'It is currently raining outside.<br>'
-	     //          		break
-		    //         case 'clear-day':
-		    //         	background.className = ''
-	     //          		background.classList.add('container','clear-day')
-	     //          		weatherStatus = 'The weather is currently clear skies.<br>'
-	     //          		break
-		    //         case 'clear-night':
-		    //         	background.className = ''
-	     //          		background.classList.add('container','clear-night')
-	     //          		weatherStatus = 'The weather is currently clear skies.<br>'
-	     //          		break
-		    //         case 'fog':
-		    //         	background.className = ''
-	     //          		background.classList.add('container','fog')
-	     //          		weatherStatus = 'The weather is currently foggy.<br>'
-	     //          		break
-		    //         case 'snow':
-		    //         	background.className = ''
-	     //          		background.classList.add('container','snow')
-	     //          		weatherStatus = 'It is currently snowing outside.<br>'
-	     //          		break
-		    //         case 'sleet':
-						// background.className = ''
-						// background.classList.add('container','sleet')
-						// weatherStatus = 'It is currently sleeting outside.<br>'
-						// break
-		    //         default:
-		    //         		background.className = ''
-	     //          		background.classList.add('container','other')
-	     //          		weatherStatus = 'Weather conditions are currently severe.<br>'
-	     //          		break
-	     //    	}
-
 	            if (unit.checked === true) { // SI units
 					temp.textContent = Math.round((data.temperature-32)*5/9) + ' °C'
 	        		forecast.innerHTML = `${weatherStatus} The temperature high for today is projected to be <strong>${Math.round((data.highs-32)*5/9)} °C</strong> and the low is projected to be <strong>${Math.round((data.lows-32)*5/9)} °C</strong>. ${data.forecast}`
@@ -150,9 +91,7 @@ const initiate = () => {
 	    	return response.json()
 		})
 		.then(data => fetchWeather(data.city))
-		.catch(error => {
-			console.log(`${error}. Too many requests. Please wait and try again later.`)
-		})
+		.catch(error => console.log(`${error}. Too many requests. Please wait and try again later.`))
 }
 initiate()
 
